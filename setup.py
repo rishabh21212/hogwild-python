cc76f6c95af6d894a8a97a14e49dced695f47739
rishabh21212 <f20191124@hyderabad.bits-pilani.ac.in>
2024-06-20 12:17:00 +0530

    docker version working with 1 error - "denied requiest to resource"

--- setup.py
+++ setup.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python
+import os
 from setuptools import setup, find_packages
 
-setup(name='hogwild',
-      url='',
-      author='',
-      package_dir={'': 'src'},
-      packages=find_packages('src'),
-      version='0.0.1',
-      install_requires=[
-            'pytest==2.9.2',
-            'grpcio==1.10.1',
-            'grpcio-tools==1.10.1',
-            'numpy==1.14.2',
-            # Ensure to remove duplicate entry if present
-      ],
-      include_package_data=True,
-      zip_safe=False)
+# Check if we are in a Docker environment
+use_docker = os.getenv('USE_DOCKER', 'false').lower() == 'true'
+
+# Set dependencies based on the environment
+if use_docker:
+    install_requires = [
+        'pytest>=7.0.0',
+        'grpcio>=1.54.0',
+        'grpcio-tools>=1.54.0',
+        'numpy>=1.24.0'
+    ]
+else:
+    install_requires = [
+        'pytest==2.9.2',
+        'grpcio==1.10.1',
+        'grpcio-tools==1.10.1',
+        'numpy==1.14.2'
+    ]
+
+setup(
+    name='hogwild',
+    url='',
+    author='',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    version='0.0.1',
+    install_requires=install_requires,
+    include_package_data=True,
+    zip_safe=False
+)
